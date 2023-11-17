0bb57066bbe5f51917b5a6cc0ddcddb41d68091e
Capucinoxx <bendertermebc@gmail.com>
2023-11-17 10:37:42 -0500

    fix(route:auth): redirection for login_view

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,11 @@ from app.database import db
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
-    print(app.config)
 
     db.init_app(app)
 
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'auth.login'
 
     @app.route('/ping')
     def ping():
