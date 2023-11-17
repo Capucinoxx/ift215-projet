72ad784a853f01799555ff43ab42c19e5993a1e5
Capucinoxx <bendertermebc@gmail.com>
2023-11-17 10:44:44 -0500

    feat(db:emotion): add emotion table

--- db/init.sql
+++ db/init.sql
@@ -5,3 +5,13 @@ CREATE TABLE "user" (
 );
 
 CREATE INDEX idx_username ON "user" (username);
+
+CREATE TABLE "emotion" (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "user" (id),
+    date DATE NOT NULL,
+    emotion VARCHAR(20) UNIQUE NOT NULL
+);
+
+CREATE INDEX idx_date ON "emotion" (date);
+CREATE INDEX idx_user_id ON "emotion" (user_id);
