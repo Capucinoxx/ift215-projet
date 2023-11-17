47769fdd567330c1dce25a530ecece3ad0965e09
Capucinoxx <bendertermebc@gmail.com>
2023-11-16 23:45:10 -0500

    chore(models:user): increase password length

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
     def set_password(self, password: str) -> None:
         self.password = generate_password_hash(password)
