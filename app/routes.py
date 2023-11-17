2e99f29dc42d662307389d8fc308f03b29dfdfa4
Capucinoxx <bendertermebc@gmail.com>
2023-11-16 23:52:16 -0500

    chore(routes:utils): add decorator for checking if json field is present

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template, redirect, url_for, flash
+from flask_login import login_user, logout_user, current_user, login_required
 from app import app
 
 
 @app.route('/ping')
 def ping():
     return 'pong'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('ping'))
