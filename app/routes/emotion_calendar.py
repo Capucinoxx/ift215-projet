aa74890633711b238b165c38c4b65ef10ea8c209
Capucinoxx <bendertermebc@gmail.com>
2023-11-17 10:53:02 -0500

    chore(routes:emotion_calendar): add business logic for retireve emotions between two dates for current_user

--- app/routes/emotion_calendar.py
+++ app/routes/emotion_calendar.py
@@ -1,5 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_login import current_user
+from app.models import Emotion
 from app.utils import check_required_json_data
+
 
 emotion_calendar = Blueprint('emotion_calendar', __name__)
 
@@ -7,7 +10,17 @@ emotion_calendar = Blueprint('emotion_calendar', __name__)
 @emotion_calendar.route('/emotions')
 @check_required_json_data(['start_date', 'end_date'])
 def emotions():
-    pass
+    data = request.json
+    start_date = data['start_date']
+    end_date = data['end_date']
+    
+    emotions = (
+        Emotion.query.filter_by(user_id=current_user.id)
+        .filter(Emotion.date.between(start_date, end_date))
+        .all()
+    )
+
+    return jsonify({'success': 'Emotions retrieved', 'data': [e.to_dict() for e in emotions]}), 200
 
 
 @emotion_calendar.route('/emotion', methods=['POST', 'DELETE'])
