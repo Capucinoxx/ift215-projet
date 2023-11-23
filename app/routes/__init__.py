3e7b7deedb2a4c2f520f4fa10a9b3bd3a2353ade
Benjamin Courchesne <42392217+Capucinoxx@users.noreply.github.com>
2023-11-23 16:36:14 -0500

    Add clicker game (#5)
    * chore(game:views): add file for mini-game
    
    * chore(game:views): add container for mini-game
    
    * feat(routes:game): add route for render circle clicker page
    
    * feat(routes:game): add page for circle game

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,9 @@
 from .auth import auth
 from .emotion_calendar import emotion_calendar
+from .game import clicker_game
 
 
 def setup_routes(app):
     app.register_blueprint(auth)
     app.register_blueprint(emotion_calendar)
+    app.register_blueprint(clicker_game)
---
+++ app/static/game.js
@@ -0,0 +1,60 @@
+const CIRCLE_SIZE = 70; // 70px;
+
+const colors = [
+    // TODO: TBD - Modify colors
+    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6'
+];
+
+(() => {
+    const container = document.getElementById('game-container');
+    if (!container)
+        return;
+
+    const counter = document.getElementById('counter');
+    if (!counter)
+        return;
+
+    let count = 0;
+
+    const generate_circle = (time) => {
+        const circle = document.createElement('div');
+        circle.classList.add('circle');
+        
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        circle.style.backgroundColor = color;
+
+        circle.style.left = `${Math.random() * (window.innerWidth - CIRCLE_SIZE)}px`;
+        circle.style.top = `${Math.random() * (window.innerHeight - CIRCLE_SIZE)}px`;
+
+        circle.addEventListener('click', () => {
+            circle.remove();
+            count++;
+            counter.innerHTML = count;
+        });
+
+        container.appendChild(circle);
+
+        fade_out_animation(circle, time);
+    };
+
+    const fade_out_animation = (circle, time) => {
+        let opacity = 1.;
+
+        const interval = setInterval(() => {
+            if (opacity <= 0) {
+                clearInterval(interval);
+                circle.remove();
+            } else {
+                circle.style.opacity = opacity;
+                opacity -= .05;
+            }
+        }, time);
+    };
+
+    let time = 0;
+    setInterval(() => {
+        const interval = Math.sin(time) * 500 + 300;
+        time += .1;
+        generate_circle(interval);
+    },2500);
+})();
