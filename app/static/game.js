1b827255a52504fe721519936bd7c409fb2e44d6
Roxane Beauregard <87456936+Rokzi@users.noreply.github.com>
2023-11-30 09:24:06 -0500

    Ajout de la musique à chaque page et fix du styling de l'accueil (#12)
    * Ajout de la musique à chaque page et fix du styling de l'accueil
    
    * fix styling journalisation
    
    ---------
    
    Co-authored-by: riom1706 <riom1706@usherbrooke.ca>

--- app/static/game.js
+++ app/static/game.js
@@ -1,60 +1,59 @@
 const CIRCLE_SIZE = 70; // 70px;
 
-const colors = [
-    // TODO: TBD - Modify colors
-    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6'
-];
+const colors = ["#FF6633", "#FFB399", "#FF33FF", "#FFFF99", "#00B3E6"];
 
 (() => {
-    const container = document.getElementById('game-container');
-    if (!container)
-        return;
+  const container = document.getElementById("game-container");
+  if (!container) return;
 
-    const counter = document.getElementById('counter');
-    if (!counter)
-        return;
+  const counter = document.getElementById("counter");
+  if (!counter) return;
 
-    let count = 0;
+  let count = 0;
 
-    const generate_circle = (time) => {
-        const circle = document.createElement('div');
-        circle.classList.add('circle');
-        
-        const color = colors[Math.floor(Math.random() * colors.length)];
-        circle.style.backgroundColor = color;
+  const generate_circle = (time) => {
+    const circle = document.createElement("div");
+    circle.classList.add("circle");
 
-        circle.style.left = `${Math.random() * (window.innerWidth - CIRCLE_SIZE)}px`;
-        circle.style.top = `${Math.random() * (window.innerHeight - CIRCLE_SIZE)}px`;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    circle.style.backgroundColor = color;
 
-        circle.addEventListener('click', () => {
-            circle.remove();
-            count++;
-            counter.innerHTML = count;
-        });
+    circle.style.left = `${
+      Math.random() * (window.innerWidth - CIRCLE_SIZE)
+    }px`;
+    circle.style.top = `${
+      Math.random() * (window.innerHeight - CIRCLE_SIZE)
+    }px`;
 
-        container.appendChild(circle);
+    circle.addEventListener("click", () => {
+      circle.remove();
+      count++;
+      counter.innerHTML = count;
+    });
 
-        fade_out_animation(circle, time);
-    };
+    container.appendChild(circle);
 
-    const fade_out_animation = (circle, time) => {
-        let opacity = 1.;
+    fade_out_animation(circle, time);
+  };
 
-        const interval = setInterval(() => {
-            if (opacity <= 0) {
-                clearInterval(interval);
-                circle.remove();
-            } else {
-                circle.style.opacity = opacity;
-                opacity -= .05;
-            }
-        }, time);
-    };
+  const fade_out_animation = (circle, time) => {
+    let opacity = 1;
 
-    let time = 0;
-    setInterval(() => {
-        const interval = Math.sin(time) * 500 + 300;
-        time += .1;
-        generate_circle(interval);
-    },2500);
+    const interval = setInterval(() => {
+      if (opacity <= 0) {
+        clearInterval(interval);
+        circle.remove();
+      } else {
+        circle.style.opacity = opacity;
+        opacity -= 0.05;
+      }
+    }, time);
+  };
+
+  let time = 0;
+  setInterval(() => {
+    const interval = Math.sin(time) * 500 + 300;
+    time += 0.1;
+    generate_circle(interval);
+  }, 2500);
 })();
